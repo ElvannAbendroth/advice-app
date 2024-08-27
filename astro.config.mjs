05ff478641511f2ff7052a32e7d4cf11b55abb14
Elvann <elvann.abendroth@gmail.com>
2024-08-27 16:59:31 +0300

    ⚙️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,25 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import netlify from '@astrojs/netlify'
-import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/serverless'
 import clerk from '@clerk/astro'
+import { shadesOfPurple } from '@clerk/themes'
 
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), clerk()],
+  integrations: [
+    tailwind(),
+    react(),
+    clerk({
+      appearance: {
+        baseTheme: [shadesOfPurple],
+        variables: { colorPrimary: '#56FDAA', colorBackground: '#313A49' },
+      },
+    }),
+  ],
   output: 'server',
   adapter: netlify(),
-  // adapter: node({ mode: 'standalone' }),
-  output: 'server',
+  adapter: vercel(),
 })
